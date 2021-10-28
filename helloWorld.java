4545938b8fed17a577603360170a92c53189834b
Shubham Agrawal <61613520+shubham-75@users.noreply.github.com>
2021-10-29 00:16:44 +0530

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class helloWorld{
     public static void main(String[] args){
-        System.out.println("Namaste World");
+        System.out.print("Hello World");
     }
 }
